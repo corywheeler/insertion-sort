0980beaef2ec9e4b7dc3a55ae78f2c630fcb971a
Cory Wheeler <corywheeler@gmail.com>
2020-09-24 20:50:58 -0400

    Initial setup

---
+++ test/bailey.spec.js
@@ -0,0 +1,12 @@
+
+describe('Bailey', () => {
+    it('should run', () => {
+        console.log('i run');
+        expect({a: 1}).to.not.have.property('b');
+        foo = 1
+            , beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
+
+        foo.should.be.a('number');
+        foo.should.equal(1);
+    })
+})
---
+++ wallaby.js
@@ -0,0 +1,15 @@
+module.exports = function() {
+    return {
+        files: ['src/**/*/*.js'],
+        tests: ['test/**/*.spec.js'],
+        env: {
+            type: 'node'
+        },
+        testFramework: 'mocha',
+        setup: function () {
+            chai.should();
+            global.expect = chai.expect;
+            global.assert = chai.assert;
+        }
+    }
+}
