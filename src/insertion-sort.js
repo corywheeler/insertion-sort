eef1956ae85bdeff80db682ef4a74777ecbbfe0d
Cory Wheeler <corywheeler@gmail.com>
2020-09-25 21:27:42 -0400

    insertion sort with specs.

--- src/insertion-sort.js
+++ src/insertion-sort.js
@@ -1,5 +1,20 @@
-const insertionSort = (unsortedList) => {
+const insertionSorter = (unsortedList) => {
+    let i, j;
+
+    for (i = 1; i < unsortedList.length; i++) {
+        j = i;
+        while ((j > 0) && (unsortedList[j] < unsortedList[j - 1])) {
+            swap(unsortedList, j, j - 1);
+            j = j - 1;
+        }
+    }
     return unsortedList;
 };
 
-module.exports = {insertionSort};
+const swap = (list, firstIndex, secondIndex) => {
+    let temp = list[firstIndex];
+    list[firstIndex] = list[secondIndex];
+    list[secondIndex] = temp;
+};
+
+module.exports = {insertionSorter, swap};
--- test/insertion-sort.spec.js
+++ test/insertion-sort.spec.js
@@ -1,9 +1,18 @@
-const {insertionSort} = require('../src/insertion-sort');
+const {insertionSorter, swap} = require('../src/insertion-sort');
 
-describe('insertion sort', () => {
+describe('insertionSorter', () => {
     it('should sort an unsorted array of integers', () => {
         let unsortedList = [3, 12, 43, 10, 100];
-        let sortedList = insertionSort(unsortedList);
-        sortedList.should.equal([3, 10, 12, 43, 100]);
+        let sortedList = insertionSorter(unsortedList);
+        sortedList.should.deep.equal([3, 10, 12, 43, 100]);
+
+    })
+})
+
+describe('swap', () => {
+    it('should swap the value of two items in an array', () => {
+        let list = [1, 2, 3];
+        swap(list, 1, 2);
+        list.should.deep.equal([1, 3, 2])
     })
 })
